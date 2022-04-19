ae862dff22f35af407326f810a778419eb388313
stu-pksp118 <stu-pksp118@AMDVLA3511GW05>
2022-04-19 12:38:45 +0300

    Внесено добавление в файл

---
+++ PM03 Menchikova/Logger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Logger
+{
+	namespace Courses
+    public static class Logger // имя своё
+	{
+		public static void WriteLog(string message)
+		{
+			string logPath = ConfigurationManager.AppSettings["logPath"];
+
+			using (StreamWriter writer = new StreamWriter(logPath, true))
+			{
+				writer.WriteLine($"{DateTime.Now} : {message}");
+			}
+}
--- PM03 Menchikova/Program.cs
+++ PM03 Menchikova/Program.cs
@@ -20,50 +20,13 @@ namespace PM03_Menchikova
             int[] x = new int[n];
  
             int add(string n, int t, double p, int s) {
-		    Courses* current, * previous, * tmp;
-		    previous = NULL;
-		    if (first) {
-			current = first;
-			do {
-				if (current->price > p) {
-					tmp = new Courses;
-					tmp->name = n;
-					tmp->price = p;
-					tmp->time = t;
-					tmp->next = current;
-					if (previous)
-						previous->next = tmp;
-					else
-						first = tmp;
-					break;
-				}
-				if (current->price == p)
-					return 0;
-				previous = current;
-				current = current->next;
-			} while (current);
-			if (!current) {
-				previous->next = new Tour;
-				previous->next->name = n;
-				previous->next->price = p;
-				previous->next->time = t;
-				previous->next->next = NULL;
-			}
-		}
-		else {
-			Courses* c = new Courses;
-			c->name = n;
-			c->price = p;
-			c->time = t;
-			c->next = NULL;
-			first = c;
-		}
-		return 1;
-	}
 
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("Введите название блюда: ",name, "Введите цену: ",price,"Введите время приготовления в минутах (от 1 до 60): ",time);
+                myArr[i].price = int.parse(console.readline());
+                Console.WriteLine("Введите название блюда: ",name);
+			    Console.WriteLine("Введите цену: ",price);
+				Console.WriteLine("Введите время приготовления в минутах (от 1 до 60): ",time);
                 x[i] = Convert.ToInt32(Console.ReadLine());
                 
             }  
@@ -72,28 +35,23 @@ namespace PM03_Menchikova
 
             
         }
+
+		for (int j = i + 1; j < nums.Length; j++)
+        {
+           if (myArr[i].price > myArr[j].price && myArr[i].time > myArr[j].time)
+           {
+              temp = nums[i];
+              nums[i] = nums[j];
+              nums[j] = temp;
+           }
+        }
+
+}
+
 string path = @"D:\Users\stu-pksp118\source\repos\Ekz_PM03_Menchikova\file.txt";   // путь к файлу
- 
-// запись в файл
-using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+ private void logger_new(string name, string action_name, TextBox textID) 
 {
-    // преобразуем строку в байты
-    byte[] buffer = Encoding.Default.GetBytes(text);
-    // запись массива байтов в файл
-    await fstream.WriteAsync(buffer, 0, buffer.Length);
-    Console.WriteLine("Текст записан в файл");
-}
- 
-// чтение из файла
-using (FileStream fstream = File.OpenRead(path))
-{
-    // выделяем массив для считывания данных из файла
-    byte[] buffer = new byte[fstream.Length];
-    // считываем данные
-    await fstream.ReadAsync(buffer, 0, buffer.Length);
-    // декодируем байты в строку
-    string textFromFile = Encoding.Default.GetString(buffer);
-    Console.WriteLine($"Текст из файла: {textFromFile}");
-}
+Logger.WriteLog(" ");
+
     }
 }
