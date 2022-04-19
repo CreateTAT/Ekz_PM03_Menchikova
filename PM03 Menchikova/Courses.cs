9b3228317d81cf37dc00ed640f86839d8bafd964
stu-pksp118 <stu-pksp118@AMDVLA3511GW05>
2022-04-19 10:19:00 +0300

    Создали класс Courses и массив с его объектами

---
+++ PM03 Menchikova/Courses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM03_Menchikova
+{
+    public class Course
+    {
+        public string name;
+        public int price;
+        public int time;
+
+        public Course(string name1, int price1, int time1)
+        {
+            name = name1;
+            price = price1;
+            time = time1;
+        }
+        public string Nazv { get; set; }
+        public int Sale { get; set; }
+        public string Time { get; set; }
+    }
+    class Courses
+    {
+        public int n;
+        public Courses(int n1)
+        {
+            n = n1;
+        }
+    }
+}
---
+++ PM03 Menchikova/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM03_Menchikova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Courses f1 = new Courses();
+            int n;
+            n = int.Parse(Console.ReadLine());
+            //создается массив с объектами класса Course
+            f1.myArr = new Course[n];
+            Console.WriteLine(f1.n);
+        }
+    }
+}
