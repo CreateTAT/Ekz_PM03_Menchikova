26c7162b5ae499449e6fc9d984f6ecc5530daf63
stu-pksp118 <stu-pksp118@AMDVLA3511GW05>
2022-04-19 10:38:46 +0300

    Добавлена функция сортировки и записи в файл

--- PM03 Menchikova/Program.cs
+++ PM03 Menchikova/Program.cs
@@ -8,6 +8,16 @@ namespace PM03_Menchikova
 {
     class Program
     {
+        void BubbleSort(int *arr, int N){
+           for (int i = 0; i < N; i++)
+              for (int j = i + 1; j < N; j++){
+                 if (arr[j] < arr[i]){
+                     int tmp = arr[i];
+                     arr[i] = arr[j];
+                     arr[j] = tmp;
+                 }
+              }
+        }
         static void Main(string[] args)
         {
             Courses f1 = new Courses();
@@ -17,5 +27,28 @@ namespace PM03_Menchikova
             f1.myArr = new Course[n];
             Console.WriteLine(f1.n);
         }
+        int main () 
+{
+    setlocale(0, "");
+    int number;
+    int *p_darr;
+    ofstream fp("number.txt"); 
+    cout << "Программа запишет целые числа в текстовый документ\n";
+    cout << "Для выхода из программы вместо числа напишите 0\n";
+    cout << "Введите целое число: ";
+    cin >> number;
+    p_darr = new int[number];
+    for (int i = 0; i < number; i++) {
+        cin >> p_darr[i];
+    }
+    BubbleSort(p_darr, number);   
+    for(int i = 0; i < number; ++i){
+       fp << p_darr[i] << ' ';
+    }
+    fp.close();
+    delete [] p_darr;
+    cout << "\033[1;31mКоманда для завершения работы принята.\033[0m\n";
+    cout << "\033[1;31mРезультат записан в number.txt.\033[0m\n";
+}
     }
 }
